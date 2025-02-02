b72509a4e9fcf2721953d8e7da78e13dd28eeb56
Rohit Anchal <105239963+rohitanchal@users.noreply.github.com>
2025-02-02 12:08:07 -0500

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-//your JS code here. If required.
 document.addEventListener("DOMContentLoaded", function() {
-  const inputs = document.querySelectorAll('.code');
-  
-  inputs.forEach((input, index) => {
-    input.addEventListener('input', (e) => {
-      const value = e.target.value;
-      if (value.length === 1 && index < inputs.length - 1) {
-        inputs[index + 1].focus();
-      }
-    });
+    const inputs = document.querySelectorAll('.code');
 
-    input.addEventListener('keydown', (e) => {
-      if (e.key === 'Backspace' && index > 0 && !inputs[index].value) {
-        inputs[index - 1].focus();
-      }
+    inputs.forEach((input, index) => {
+        input.addEventListener('input', (e) => {
+            const value = e.target.value;
+            if (value.length === 1 && index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
+        });
+
+        input.addEventListener('keydown', (e) => {
+            if (e.key === 'Backspace' && !inputs[index].value && index > 0) {
+                inputs[index - 1].focus();
+            }
+        });
     });
-  });
 });
