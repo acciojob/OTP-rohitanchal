7be25eb4b2358f1cf457c66130da320624451019
Rohit Anchal <105239963+rohitanchal@users.noreply.github.com>
2025-02-02 12:17:06 -0500

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     const inputs = document.querySelectorAll('.code');
 
+    // Focus on first input when page loads
+    inputs[0].focus();
+
     inputs.forEach((input, index) => {
         input.addEventListener('input', (e) => {
-            const value = e.target.value;
-            if (value.length === 1 && index < inputs.length - 1) {
-                inputs[index + 1].focus();
+            if (e.target.value.length === 1) {
+                if (index < inputs.length - 1) {
+                    inputs[index + 1].focus();
+                }
             }
         });
 
         input.addEventListener('keydown', (e) => {
-            if (e.key === 'Backspace' && !inputs[index].value && index > 0) {
+            if (e.key === 'Backspace' && !input.value && index > 0) {
                 inputs[index - 1].focus();
             }
         });
